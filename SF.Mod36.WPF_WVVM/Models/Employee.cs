651883c73a7350f08157dd7d5d01ec2b3d94f103
Anatolii Ivanov <gotnickname@gmail.com>
2023-11-20 20:38:31 +0530

    Add project files.

---
+++ SF.Mod36.WPF_WVVM/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SF.Mod36.WPF_WVVM.Models;
+
+public class Employee
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string CompanyName { get; set; }
+    public string JobTitle { get; set; }
+    public string CityName { get; set; }
+}
---
+++ SF.Mod36.WPF_WVVM/ViewModels/EmployeesViewModel.cs
@@ -0,0 +1,28 @@
+﻿using SF.Mod36.WPF_WVVM.DataAcces;
+using SF.Mod36.WPF_WVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF.Mod36.WPF_WVVM.ViewModels;
+
+public class EmployeesViewModel
+{
+	private readonly EmployeeRepository _repository;
+
+	public EmployeesViewModel()
+	{
+		_repository = new EmployeeRepository();
+	}
+
+	public ObservableCollection<Employee> Employees
+	{
+		get
+		{
+			return new ObservableCollection<Employee>(this._repository.GetAll());
+		}
+	}
+}
