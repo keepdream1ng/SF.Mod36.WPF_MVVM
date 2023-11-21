6cef3d233443efa23896b29f06e689f4cf9967be
Anatolii Ivanov <gotnickname@gmail.com>
2023-11-21 15:23:40 +0530

    two way binding works

--- SF.Mod36.WPF_WVVM/ViewModels/EmployeesViewModel.cs
+++ SF.Mod36.WPF_WVVM/ViewModels/EmployeesViewModel.cs
@@ -3,26 +3,62 @@ using SF.Mod36.WPF_WVVM.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SF.Mod36.WPF_WVVM.ViewModels;
 
-public class EmployeesViewModel
+public class EmployeesViewModel : INotifyPropertyChanged
 {
 	private readonly EmployeeRepository _repository;
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	private string _filter;
+	public string Filter {
+		get { return _filter; }
+		set
+		{
+			if (_filter != value)
+			{
+				_filter = value;
+				FillListView();
+			}
+		}
+	}
+	public ObservableCollection<Employee> Employees { get; set; }
 
 	public EmployeesViewModel()
 	{
 		_repository = new EmployeeRepository();
+		FillListView();
 	}
 
-	public ObservableCollection<Employee> Employees
+	private void OnPropertyChanged([CallerMemberName]string propertyName = null)
 	{
-		get
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+
+	private void FillListView()
+	{
+		if (!String.IsNullOrEmpty(Filter))
 		{
-			return new ObservableCollection<Employee>(this._repository.GetAll());
+			Employees = new ObservableCollection<Employee>(
+			  _repository.GetAll()
+				.Where(v => 
+						(v.FirstName + v.LastName)
+						.ToLower()
+						.Contains(Filter.ToLower())
+				)
+			  );
 		}
+		else
+		{
+			Employees = new ObservableCollection<Employee>(_repository.GetAll());
+		}
+		OnPropertyChanged(nameof(Employees));
 	}
 }
